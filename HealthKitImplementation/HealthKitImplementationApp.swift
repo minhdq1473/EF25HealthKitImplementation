84d74a0ef51d8a71e1661bb8e74678419c2b8784
minhdq1473 <minhdoquang1473@gmail.com>
2025-08-15 09:19:44 +0700

    Initial Commit

---
+++ HealthKitImplementation/HealthKitImplementationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitImplementationApp.swift
+//  HealthKitImplementation
+//
+//  Created by iKame Elite Fresher 2025 on 15/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitImplementationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
